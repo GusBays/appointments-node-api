169580c8cdb85d253aef85caf660c77045548ebf
Gustavo Bays <dooca@MacBook-Pro-de-Gustavo.local>
2023-03-31 22:35:58 -0300

    fix: removed unecessary imports

--- src/Modules/Plans/Domain/PlanRepositoryInterface.ts
+++ src/Modules/Plans/Domain/PlanRepositoryInterface.ts
@@ -1,7 +1,6 @@
 import Plan from "../../../models/Plan";
-import { PlanData } from "./PlanData";
 
 export interface PlanRepositoryInterface
 {
-    create(planData: PlanData): Promise<Plan>
+    create(plan: Object): Promise<Plan>
 }
